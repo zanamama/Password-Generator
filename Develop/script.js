286ad4f8c2ddd30751d8a08da40f0b80cc096fcd
zanama <57731386+zanmama@users.noreply.github.com>
2021-07-17 21:03:51 -0400

    added commands, removed if-else statements that broke the code

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,21 @@
+// Pseudo Code 
+// 1. Password Generator gives us a random password
+// 2. User Specifies the password criteria
+//         - Have user choose between:
+//            - Length of Password (8-128)
+//            - Upper Case
+//            - Special Characters
+//            - Numbers
+//            - Lower Case
+// 3. Computer derives a random password with user's specifications.
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lowerCaseArr = ["a" , "b" , "c" , "d" , "e"];
-var upperCaseArr = ["A" , "B" , "C" , "D" , "E"];
-var specialCharArr = ["!" , "@" , "#"];
-var numberArr = ["1" , "2" , "3" , "4" , "5"];
+var lowerCaseArr = ["a" , "b" , "c" , "d" , "e", "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q"];
+var upperCaseArr = ["A" , "B" , "C" , "D" , "E" , "F" , "G" , "H" , "I" , "J" , "K" , "L" , "M" , "N" , "O" , "P" , "Q"];
+var specialCharArr = ["!" , "@" , "$", "#" , "%" , "^" , "&" , "*" , "("];
+var numberArr = ["1" , "2" , "3" , "4" , "5", "6" , "7" , "8" , "9" , "0"];
+
 
 // Write password to the #password input
 function writePassword() {
@@ -16,42 +28,34 @@ function writePassword() {
 
 function generatePassword() {
   //// presented with a series of prompts for password criteria
-    var length = prompt("length of pswd . must be between 8 and 128");
-   // console.log(length)
-
-  if (length < 8 ) {
-    console.log('length too short');
-    return;
-  } 
-  if (length > 128) {
-    console.log ("length too long");
-    return;
-}
-useLowerCase()
-console.log(length)
+  receivePasswordLength();
 }
 
-function useLowerCase() {
-  //// presented with a series of prompts for password criteria
-    var useLowerCaseResponse = confirm("use lowercase?");
+function receivePasswordLength() {
+  var passwordLength = prompt ("What password length do you want? Must choose between 8-128");
+  return passwordLength;
 }
-function useLowerCase() {
-  //// presented with a series of prompts for password criteria
-    var useLowerCaseResponse = confirm("use lowercase?");
-    return;
-}
-console.log(length)
 
-// questionable code below. breaks pw generator
-// function  () {
-  
-//}
-//if (lowerCaseArr==true) {
-//  console.log ("okay");
-//  return;
-//}
-//console.log(lowerCaseArr)
-//}
+function includeUpperCase() {
+  var useUpperCase = confirm ("Would you like to include uppercase letters?");
+  return useUpperCase;
+}
+
+function includeLowerCase() {
+  var useLowerCase = confirm ("Would you like to include lowercase letters?");
+  return useLowerCase;
+}
+
+function includeSpecialCharacters() {
+  var useSpecialCharacters = confirm ("Would you like to include special characters?");
+  return useSpecialCharacters;
+}
+
+function includeNumbers() {
+  var useNumbers = confirm ("Would you like to incldue numbers?");
+  return useNumbers;
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
